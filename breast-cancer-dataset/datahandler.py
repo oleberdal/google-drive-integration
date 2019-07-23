81386f7ddfcd32833e92d5d119c7aa18533c6121
Ole Berdal <oleberdal@gmail.com>
2019-07-23 11:33:33 +0200

    Minor improvements.

--- breast-cancer-dataset/datahandler.py
+++ breast-cancer-dataset/datahandler.py
@@ -3,25 +3,7 @@ import random
 import connector
 
 
-def fuzzifier1(value):
-    v = int(value)
-    classes = ['thin', 'normal', 'thick', 'very thick']
-    class1 = random.uniform(0, min(1, max(0, 1.25 - 0.5 * v)))
-    class2 = random.uniform(0, min(min(max(-0.25 + 0.5 * v, 0), 1), max(min(3.25 - 0.5 * v, 1), 0)))
-    class3 = random.uniform(0, min(min(max(-1.75 + 0.5 * v, 0), 1), max(min(4.75 - 0.5 * v, 1), 0)))
-    class4 = random.uniform(0, min(max(-3.25 + 0.5 * v, 0), 1))
-    winner = max(class1, class2, class3, class4)
-    if winner == class1:
-        return classes[0]
-    elif winner == class2:
-        return classes[1]
-    elif winner == class3:
-        return classes[2]
-    else:
-        return classes[3]
-
-
-def fuzzifier2(value):
+def fuzzifier(value):
     v = int(value)
     classes = ['thin', 'normal', 'thick', 'very thick']
     class1 = random.uniform(0, min(1, max(0, 1.5 - v)))
@@ -50,8 +32,8 @@ def main():
 
     worksheet.update_cell(1, len(all_records[0]), 'clump_thickness_cat2')
     for row, data in enumerate(all_records[1:]):
-        worksheet.update_cell(row + 2, len(data), fuzzifier2(value=data[headers.index('clump_thickness')]))
-        time.sleep(1.1)
+        worksheet.update_cell(row + 2, len(data), fuzzifier(value=data[headers.index('clump_thickness')]))
+        time.sleep(1)
 
 
 if __name__ == '__main__':
